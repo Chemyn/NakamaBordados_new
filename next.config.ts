f6372a595ee282f4bb23a426c9efef95d76ec081
chemyn <josemlopez2310@gmail.com>
2026-05-26 15:56:08 -0700

    build: setup automatic FTP deployment for staging

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   /* config options here */
   // @ts-ignore - Dev origins for HMR over port forwarding
   allowedDevOrigins: ['localhost', '192.168.0.241', '189.198.139.6', '*'],
